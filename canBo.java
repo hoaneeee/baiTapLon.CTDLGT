39cafb55440714e0daca65b571742243ca517f8f
hoaneeee <138707064+hoaneeee@users.noreply.github.com>
2024-03-25 21:43:57 +0700

    Add files via upload

---
+++ canBo.java
@@ -0,0 +1,30 @@
+package baiTapLon2;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class canBo {
+    int maCanBo;
+    String tenCanBo;
+    String phongBan;
+    double tongLuong;
+    canBo next;
+
+    Locale locate= new Locale("vn","VN");
+    NumberFormat nf= NumberFormat.getCurrencyInstance(locate);
+
+    public canBo(int maCanBo, String tenCanBo, String phongBan, double tongLuong) {
+        this.maCanBo = maCanBo;
+        this.tenCanBo = tenCanBo;
+        this.phongBan = phongBan;
+        this.tongLuong = tongLuong;
+        this.next =null;
+    }
+
+    public void output(){
+        System.out.println(maCanBo);
+        System.out.println(tenCanBo);
+        System.out.println(phongBan);
+        System.out.println(nf.format(tongLuong));
+    }
+}
---
+++ test.java
@@ -0,0 +1,59 @@
+package baiTapLon2;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        quanLiCanBo ql =new quanLiCanBo();
+        int choice;
+        do {
+            System.out.println("-----Menu-----");
+            System.out.println("1. Tạo danh sách cán bộ");
+            System.out.println("2. Thêm cán bộ vào danh sách");
+            System.out.println("3. Liệt kê danh sách cán bộ");
+            System.out.println("4. Thống kê số lượng cán bộ theo phòng ban");
+              System.out.println("5. In cán bộ có tổng lương >= 5000000");
+            System.out.println("6. Tìm vị trí cán bộ theo mã số");
+            System.out.println("7. Tìm kiếm cán bộ theo tên");
+            System.out.println("8. Sắp xếp danh sách cán bộ theo tổng lương giảm dần");
+            System.out.println("0. Thoát chương trình");
+            System.out.print("Nhập lựa chọn của bạn: ");
+            choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    ql. taoDanhSach();
+                    break;
+                case 2:
+                    ql.addCanBo();
+                    break;
+                case 3:
+                    ql.lietkeDanhSach();
+                    break;
+                case 4:
+                    ql.thongkeCacBan();
+                    break;
+                case 5:
+                    ql.luongLonHon1Jack();
+                    break;
+                case 6:
+                    ql.searchMCB();
+                    break;
+                case 7:
+                    ql.searchTCB();
+                    break;
+                case 8:
+                    ql.sortLuongGiamDan();
+                    break;
+                case 0:
+                    System.out.println("Thoát chương trình.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    break;
+            }
+        }while (choice!=0);
+
+
+    }
+}
